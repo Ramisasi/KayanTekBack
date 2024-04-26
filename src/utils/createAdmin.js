5c993f9f78a39580a4c0ccb1f2ca47942648ee22
Ramisasi <ramisase@gmail.com>
2024-04-26 11:18:30 +0200

    update env

--- src/utils/createAdmin.js
+++ src/utils/createAdmin.js
@@ -2,30 +2,34 @@ import { userModel } from "../DB/model/user.model.js";
 import { apiError, asyncHandle } from "../middleware/errorHandle.js";
 import bcrypt from "bcryptjs";
 
-export const createAdmin = asyncHandle(async () => {
-  const user = await userModel.findOne({
-    userName: process.env.adminName,
-    isDeleted: false,
-  });
-  if (!user) {
-    bcrypt.hash(
-      process.env.userPassword,
-      parseInt(process.env.saltRand),
-      async (err, result) => {
-        if (err || !result) {
-          return console.log("in-valid hash Password");
-        } else {
-          const createUser = new userModel({
-            userName: process.env.adminName,
-            password: result,
-          });
-          const createdUser = await createUser.save();
-          console.log({
-            _id: createdUser._id,
-            userName: createdUser.userName,
-          });
+export const createAdmin = async () => {
+  try {
+    const user = await userModel.findOne({
+      userName: process.env.adminName,
+      isDeleted: false,
+    });
+    if (!user) {
+      bcrypt.hash(
+        process.env.userPassword,
+        parseInt(process.env.saltRand),
+        async (err, result) => {
+          if (err || !result) {
+            return console.log("in-valid hash Password");
+          } else {
+            const createUser = new userModel({
+              userName: process.env.adminName,
+              password: result,
+            });
+            const createdUser = await createUser.save();
+            console.log({
+              _id: createdUser._id,
+              userName: createdUser.userName,
+            });
+          }
         }
-      }
-    );
+      );
+    }
+  } catch (error) {
+    return new apiError("catch error", 500);
   }
-});
+};
